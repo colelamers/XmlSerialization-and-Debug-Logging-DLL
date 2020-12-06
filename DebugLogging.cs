756d86646b7a4ba08f005baaf9956fab4746b2f8
Cole Lamers <colelamers@outlook.com>
2020-12-06 14:31:33 -0600

    Initial Commit of ProjectLogging files

---
+++ DebugLogging.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace ProjectLogging
+{
+    /*
+     * Created by Cole Lamers 
+     * Date: 2020-12-06
+     * 
+     * == Purpose ==
+     * This code sets up the debug logging library to be used  
+     * 
+     * Changes: (date, comment)
+     * 
+     */
+
+    /*
+     * == Global Task List ==
+     * 
+     */
+    public class DebugLogging
+    {
+        static string LogFilePath { get; set; }
+        static string LogFileName { get; set; }
+        static string LogFilePathAndName { get; set; }
+
+        /* Commented out constructor because I'm calling the CreateDebugLogger() instead to initialize it
+        public DebugLogging()
+        {
+            CreateDebugLogger();
+        }
+        */
+
+    public static void CreateDebugLogger()
+        {
+            LogFilePath = @"..\Debug\Logs\";
+            LogFileName = $"{DateTime.Now:yyyyMMdd}_Log.txt";
+            LogFilePathAndName = Path.Combine(LogFilePath, LogFileName);
+
+            if (!Directory.Exists(LogFilePath))
+            {//TODO: --2-- need to fix this so that it doesn't check if a directory exist but it just makes it instead.
+                Directory.CreateDirectory(LogFilePath);
+            }//creates the log directory if it doesn't exist
+
+            if (!File.Exists(Path.GetFullPath(LogFilePathAndName)))
+            {
+                using (StreamWriter sw = File.CreateText(LogFilePathAndName)) { }//just creates and closes the file if it does not exist
+            }//creates the debug file for specific day the program is run
+        }
+
+        public static void LogActivity(string status)
+        {
+            using (StreamWriter streamWriter = File.AppendText(Path.GetFullPath(LogFilePathAndName)))
+            {
+                streamWriter.WriteLine($"{DateTime.Now:yyyy:MM:dd HH:mm:ss.ffff}, {status}");
+            }
+        }//writes to the debug logging file
+    }
+}
---
+++ SetupConfigFile.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace ProjectLogging
+{
+    public class SetupConfigFile
+    {
+        /*
+         * Created by Cole Lamers 
+         * Date: 2020-12-06
+         * 
+         * == Purpose ==
+         * This code sets up the XML serialization so that any configuration class created
+         * can be easily passed through and referenced directly.    
+         * 
+         * Changes: (date, comment)
+         * 
+         */
+
+        /* 
+         * == Global Task List ==
+         * TODO: --2-- Need to work on a function that sets the default path/location when saving/generating the file
+         * TODO: --4-- rework file naming for GetProjectName()? maybe not go off of project? not a priority or major.
+         */
+
+        public static void LoadSaveFile<ConfigClass>(ref ConfigClass config)
+        {
+            //TODO: --1-- think of a better way to make this
+            SaveToFile(ref config);
+            LoadFromFile(ref config);
+        }//Combines both actions of save and load so both do not need to be called when program is initially run
+
+        public static void SaveToFile<ConfigClass>(ref ConfigClass config)
+        {
+            if (!File.Exists(Path.GetFullPath(GetProjectName())))
+            {
+                using (StreamWriter sw = File.CreateText(GetProjectName())) { }
+            }//Creates the config file if it doesn't exist
+            using (var stream = new FileStream(GetProjectName(), FileMode.Create))
+            {
+                XmlSerializer XML = new XmlSerializer(typeof(ConfigClass));
+                XML.Serialize(stream, config);
+            }//Serializes the object type passed through
+        }//Passes through a refernce to a class, generates the serializer that way
+
+        public static void LoadFromFile<ConfigClass>(ref ConfigClass config)
+        {
+            using (var stream = new FileStream(GetProjectName(), FileMode.Open))
+            {
+                var XML = new XmlSerializer(typeof(ConfigClass));
+                config = (ConfigClass)XML.Deserialize(stream);
+            }
+        }//Loads in the existing referenced config file
+
+        public static string GetProjectName()
+        {
+            return System.Reflection.Assembly.GetCallingAssembly().GetName().Name + "_Config.xml";
+        }//Creates a config file based on the project assembly name
+    }
+}
